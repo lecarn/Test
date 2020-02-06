2390158667b341001137c209451055c316b3a0ae
Marcus <zuperdorf@gmail.com>
2020-02-06 12:41:38 +0100

    commited

---
+++ src/TestGit/Area.java
@@ -0,0 +1,11 @@
+package TestGit;
+
+public class Area {
+
+    public double CircleArea(double a){
+        return a * a * Math.PI;
+    }
+    public double SquareArea(double a, double b){
+        return a * b;
+    }
+}
---
+++ src/TestGit/AreaTest.java
@@ -0,0 +1,11 @@
+package TestGit;
+
+public class AreaTest {
+
+    public static void main(String[]args){
+        Area areaCalc = new Area();
+
+        System.out.println(areaCalc.CircleArea(5));
+        System.out.println(areaCalc.SquareArea(4, 5));
+    }
+}
